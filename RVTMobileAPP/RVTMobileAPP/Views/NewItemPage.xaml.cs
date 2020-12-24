6f07bc7699768dbdfa72ac966913a0b34354dc17
cristian.ionas <cris.ionas@gmail.com>
2020-12-24 19:50:44 +0200

    Project Init

---
+++ RVTMobileAPP/RVTMobileAPP/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RVTMobileAPP.Models;
+using RVTMobileAPP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RVTMobileAPP.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
